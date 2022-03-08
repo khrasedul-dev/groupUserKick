46188eaa7f56f9e7fa71ff9735bbeb9f570fbcde
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-03-08 20:17:27 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const { Telegraf , Composer } = require('micro-bot')
+
+const bot = new Composer()
+
+bot.on('new_chat_members',ctx=>{
+    const f = ctx.from.first_name || '0'
+    const l = ctx.from.last_name || '0'
+
+
+    const fn = f.toString()
+    const fl = fn.length
+
+    const ln = l.toString()
+    const ll = ln.length
+
+    const namelength = fl+ll
+
+    console.log(namelength)
+
+    if(namelength> 35){
+        ctx.kickChatMember(ctx.from.id).then((data)=>console.log('User Removed')).catch((e)=>console.log(e))
+    }
+
+})
+
+
+
+module.exports = bot
