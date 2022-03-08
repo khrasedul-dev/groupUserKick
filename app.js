2180827cd9a4beded7058d516a8df01a8a12e3dc
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-03-08 21:16:35 +0600

    Update app.js

--- app.js
+++ app.js
@@ -24,5 +24,27 @@ bot.on('new_chat_members',ctx=>{
 })
 
 
+bot.on('text',ctx=>{
+    const f = ctx.from.first_name || '0'
+    const l = ctx.from.last_name || '0'
+
+
+    const fn = f.toString()
+    const fl = fn.length
+
+    const ln = l.toString()
+    const ll = ln.length
+
+    const namelength = fl+ll
+
+    console.log(namelength)
+
+    if(namelength> 35){
+        ctx.kickChatMember(ctx.from.id).then((data)=>console.log('User Removed')).catch((e)=>console.log(e))
+    }
+
+})
+
+
 
 module.exports = bot
